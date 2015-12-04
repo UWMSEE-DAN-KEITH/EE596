748f8a15876aae555c37514f11f419691e28663a
DanSweet3 <dan.sweet3@gmail.com>
2015-12-04 22:45:01 +0000

    Converted h264 video to mp4 and added to video directory, fixed a couple minor issues in motion _detector.py so it now runs on the Pi

--- motion_detector.py
+++ motion_detector.py
@@ -31,35 +31,35 @@ avg = None
 
 # loop over the frames of the video
 while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    (grabbed, frame) = camera.read()
-    text = "Unoccupied"
+	# grab the current frame and initialize the occupied/unoccupied
+	# text
+	(grabbed, frame) = camera.read()
+	text = "Unoccupied"
  
 	# if the frame could not be grabbed, then we have reached the end
 	# of the video
 	if not grabbed:
-        print "frame could not be grabbed"
+		print "frame could not be grabbed"
 		break
- 
+	
 	# resize the frame, convert it to grayscale, and blur it
 	frame = imutils.resize(frame, width=500)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
- 
+	
 	# if the average frame is None, initialize it
 	if avg is None:
 		print "[INFO] starting background model..."
 		avg = gray.copy().astype("float")
 		#rawCapture.truncate(0)
 		continue
- 
+	
 	# accumulate the weighted average between the current frame and
 	# previous frames, then compute the difference between the current
 	# frame and running average
 	cv2.accumulateWeighted(gray, avg, 0.5)
 	frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
+	
 	# threshold the delta image, dilate the thresholded image to fill
 	# in holes, then find contours on thresholded image
 	thresh = cv2.threshold(frameDelta, 5, 255,
@@ -67,33 +67,32 @@ while True:
 	thresh = cv2.dilate(thresh, None, iterations=2)
 	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
- 
+	
 	# loop over the contours
 	for c in cnts:
 		# if the contour is too small, ignore it
 		if cv2.contourArea(c) < args["min_area"]:
 			continue
- 
+	
 		# compute the bounding box for the contour, draw it on the frame,
 		# and update the text
 		(x, y, w, h) = cv2.boundingRect(c)
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		text = "Occupied"
-
+	
 	# draw the text and timestamp on the frame
 	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
 		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
- 
+	
 	# show the frame and record if the user presses a key
 	cv2.imshow("Security Feed", frame)
 	cv2.imshow("Thresh", thresh)
 	cv2.imshow("Frame Delta", frameDelta)
-
-	print "waiting for keypress"
+	
 	key = cv2.waitKey(1) & 0xFF
- 
+	
 	# if the `q` key is pressed, break from the lop
 	if key == ord("q"):
 		break
