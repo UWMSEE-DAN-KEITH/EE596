2e85fef5c5ebaf6b8ebdc2ea8549e361c1b5c73b
DanSweet3 <dan.sweet3@gmail.com>
2015-12-06 11:27:44 -0800

    Added moveWindow command to the display windows so now they should show up in a repeatable location (and not end up off screen). Also fixed some tabbing issues. I think Eclipse is using 4 space tabs but the Raspberry Pi is using actual tabs, so things break when moving between the two. I converted this whole file to 4 space tabs.

--- motion_detector.py
+++ motion_detector.py
@@ -3,13 +3,15 @@ import argparse
 import datetime
 import imutils
 import time
-from picamera import PiCamera
+#from picamera import PiCamera
 import cv2
- 
+
+WINDOW_SIZE = 500;
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-a", "--min-area", type=int, default=WINDOW_SIZE, help="minimum area size")
 args = vars(ap.parse_args())
  
 # if the video argument is None, then we are reading from webcam
@@ -31,71 +33,74 @@ avg = None
 
 # loop over the frames of the video
 while True:
-	# grab the current frame and initialize the occupied/unoccupied
-	# text
-	(grabbed, frame) = camera.read()
-	text = "Unoccupied"
+    # grab the current frame and initialize the occupied/unoccupied
+    # text
+    (grabbed, frame) = camera.read()
+    text = "Unoccupied"
  
-	# if the frame could not be grabbed, then we have reached the end
-	# of the video
-	if not grabbed:
-		print "frame could not be grabbed"
-		break
+    # if the frame could not be grabbed, then we have reached the end
+    # of the video
+    if not grabbed:
+        print "frame could not be grabbed"
+        break
 	
-	# resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width=500)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    # resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width=WINDOW_SIZE)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 	
-	# if the average frame is None, initialize it
-	if avg is None:
-		print "[INFO] starting background model..."
-		avg = gray.copy().astype("float")
-		#rawCapture.truncate(0)
-		continue
+    # if the average frame is None, initialize it
+    if avg is None:
+        print "[INFO] starting background model..."
+        avg = gray.copy().astype("float")
+        #rawCapture.truncate(0)
+        continue
 	
-	# accumulate the weighted average between the current frame and
-	# previous frames, then compute the difference between the current
-	# frame and running average
-	cv2.accumulateWeighted(gray, avg, 0.5)
-	frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 	
-	# threshold the delta image, dilate the thresholded image to fill
-	# in holes, then find contours on thresholded image
-	thresh = cv2.threshold(frameDelta, 5, 255,
-		cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations=2)
-	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, 5, 255,
+        cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
 	
 	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < args["min_area"]:
-			continue
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
 	
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Occupied"
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Occupied"
 	
-	# draw the text and timestamp on the frame
-	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    # draw the text and timestamp on the frame
+    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+ 
+    # show the frame and record if the user presses a key
+    cv2.imshow("Security Feed", frame)
+    cv2.moveWindow("Security Feed", 0, 100)
+    cv2.imshow("Thresh", thresh)
+    cv2.moveWindow("Thresh", WINDOW_SIZE+25, 100)
+    cv2.imshow("Frame Delta", frameDelta)
+    cv2.moveWindow("Frame Delta", 2*WINDOW_SIZE+50, 100)
 	
-	# show the frame and record if the user presses a key
-	cv2.imshow("Security Feed", frame)
-	cv2.imshow("Thresh", thresh)
-	cv2.imshow("Frame Delta", frameDelta)
+    key = cv2.waitKey(1) & 0xFF
 	
-	key = cv2.waitKey(1) & 0xFF
-	
-	# if the `q` key is pressed, break from the lop
-	if key == ord("q"):
-		break
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
  
 # cleanup the camera and close any open windows
 camera.release()
